fb82b40e631ba27b005c5f165584b212291160c9
M Sukri <142651234+Msxky@users.noreply.github.com>
2023-10-20 22:16:40 +0700

    Add files via upload

---
+++ deskripsi-uang/deskripsi.js
@@ -0,0 +1,127 @@
+// UANG 500
+let uang500= document.getElementById('uang500')
+const audioDepan1 = new Audio("../deskripsi-uang/assets/audio/uang-500-depan.mp3");
+function Depan1() {
+  audioDepan1.play()
+  audioBelakang1.pause()
+  uang500.src='../deskripsi-uang/assets/img/500-depan.jpg'
+}
+
+const audioBelakang1 = new Audio("../deskripsi-uang/assets/audio/uang-500-belakang.mp3");
+function Belakang1(){
+  audioBelakang1.play()
+  audioDepan1.pause()
+  uang500.src='../deskripsi-uang/assets/img/500-belakang.jpg'
+}
+
+// UANG 1000
+let uang1000= document.getElementById('uang1000')
+const audioDepan2 = new Audio("../deskripsi-uang/assets/audio/uang-1000-depan.mp3");
+function Depan2() {
+  audioDepan2.play()
+  audioBelakang2.pause()
+  uang1000.src='../deskripsi-uang/assets/img/1000-depan.jpg'
+}
+
+const audioBelakang2 = new Audio("../deskripsi-uang/assets/audio/uang-1000-belakang.mp3");
+function Belakang2(){
+  audioBelakang2.play()
+  audioDepan2.pause()
+  uang1000.src='../deskripsi-uang/assets/img/1000-belakang.jpg'
+}
+
+// UANG 2000
+let uang2000= document.getElementById('uang2000')
+const audioDepan3 = new Audio("../deskripsi-uang/assets/audio/uang-2000-depan.mp3");
+function Depan3() {
+  audioDepan3.play()
+  audioBelakang3.pause()
+  uang2000.src='../deskripsi-uang/assets/img/2000-depan.jpg'
+}
+
+const audioBelakang3 = new Audio("../deskripsi-uang/assets/audio/uang-2000-belakang.mp3");
+function Belakang3(){
+  audioBelakang3.play()
+  audioDepan3.pause()
+  uang2000.src='../deskripsi-uang/assets/img/2000-belakang.jpg'
+}
+
+// UANG 5000
+let uang5000= document.getElementById('uang5000')
+const audioDepan4 = new Audio("../deskripsi-uang/assets/audio/uang-5000-depan.mp3");
+function Depan4() {
+  audioDepan4.play()
+  audioBelakang4.pause()
+  uang5000.src='../deskripsi-uang/assets/img/5000-depan.jpg'
+}
+
+const audioBelakang4 = new Audio("../deskripsi-uang/assets/audio/uang-5000-belakang.mp3");
+function Belakang4(){
+  audioBelakang4.play()
+  audioDepan4.pause()
+  uang5000.src='../deskripsi-uang/assets/img/5000-belakang.jpg'
+}
+
+// UANG 10.000
+let uang10_000= document.getElementById('uang10_000')
+const audioDepan5 = new Audio("../deskripsi-uang/assets/audio/uang-10.000-depan.mp3");
+function Depan5() {
+  audioDepan5.play()
+  audioBelakang5.pause()
+  uang10_000.src='../deskripsi-uang/assets/img/10.000-depan.jpg'
+}
+
+const audioBelakang5 = new Audio("../deskripsi-uang/assets/audio/uang-10.000-belakang.mp3");
+function Belakang5(){
+  audioBelakang5.play()
+  audioDepan5.pause()
+  uang10_000.src='../deskripsi-uang/assets/img/10.000-belakang.jpg'
+}
+
+// UANG 20.000
+let uang20_000= document.getElementById('uang20_000')
+const audioDepan6 = new Audio("../deskripsi-uang/assets/audio/uang-20.000-depan.mp3");
+function Depan6() {
+  audioDepan6.play()
+  audioBelakang6.pause()
+  uang20_000.src='../deskripsi-uang/assets/img/20.000-depan.jpg'
+}
+
+const audioBelakang6 = new Audio("../deskripsi-uang/assets/audio/uang-20.000-belakang.mp3");
+function Belakang6(){
+  audioBelakang6.play()
+  audioDepan6.pause()
+  uang20_000.src='../deskripsi-uang/assets/img/20.000-belakang.jpg'
+}
+
+// UANG 50.000
+let uang50_000= document.getElementById('uang50_000')
+const audioDepan7 = new Audio("../deskripsi-uang/assets/audio/uang-50.000-depan.mp3");
+function Depan7() {
+  audioDepan7.play()
+  audioBelakang7.pause()
+  uang50_000.src='../deskripsi-uang/assets/img/50.000-depan.jpg'
+}
+
+const audioBelakang7 = new Audio("../deskripsi-uang/assets/audio/uang-50.000-belakang.mp3");
+function Belakang7(){
+  audioBelakang7.play()
+  audioDepan7.pause()
+  uang50_000.src='../deskripsi-uang/assets/img/50.000-belakang.jpg'
+}
+
+// UANG 100.000
+let uang100_000= document.getElementById('uang100_000')
+const audioDepan8 = new Audio("../deskripsi-uang/assets/audio/uang-100.000-depan.mp3");
+function Depan8() {
+  audioDepan8.play()
+  audioBelakang8.pause()
+  uang100_000.src='../deskripsi-uang/assets/img/100.000-depan.jpg'
+}
+
+const audioBelakang8 = new Audio("../deskripsi-uang/assets/audio/uang-100.000-belakang.mp3");
+function Belakang8(){
+  audioBelakang8.play()
+  audioDepan8.pause()
+  uang100_000.src='../deskripsi-uang/assets/img/100.000-belakang.jpg'
+}
